970d21f19675abd9dad5e91ff260ea67ceb80de6
Yavkata <qshilukanov213@gmail.com>
2025-12-06 16:52:44 +0200

    Project update

--- BattleShips.cpp
+++ BattleShips.cpp
@@ -8,13 +8,14 @@ using namespace std;
 
 void saveGameStateToFile(char[15][15], char[15][15], int, bool);
 
-void loadGameFromFile();
+void loadGameFromFile(char[15][15], char[15][15], int, bool);
 
 void startNewGame(char[15][15], char[15][15], int, bool, bool);
 
-void printUI();
+void printUI(char[15][15], char[15][15], int);
 
 int main() {
+    srand(time(0));
     //1. Choose to load/start new game
     //2. Choose difficulty: 
     //      - Calm Waters (дъска 10x10)
@@ -35,11 +36,20 @@ int main() {
 
     //TODO: load or start new game depending on choice; automatic player input
 
-    bool manualPlayerInput = true;
+    bool manualPlayerInput = false;
 
     startNewGame(computerBoard, playerBoard, boardSize, manualPlayerInput, isPlayerTurn);
 
-    printUI();
+    printUI(computerBoard, playerBoard, boardSize);
+
+    bool isGameOver = false;
+
+    //TODO: game cycle; playTurn(..., isPlayerTurn); at the end of each turn check if the game is ended; when end, restart
+
+    //retry input if invalid
+    while (!isGameOver) {
+        printUI(computerBoard, playerBoard, boardSize);
+    }
 }
 
 void isValidPlacement(char board[15][15], int shipLength, bool isHorizontal) {
@@ -50,6 +60,14 @@ void automaticallyFillBoard(char board[15][15], int boardSize) {
     //TODO: depending on board size place: 4 - 1 tile ships, 3 - 2 tile ships, 2 - 3 tile ships, 1 - 4 tile ship
 
     //TODO: generate a random tile in the board and check if valid
+
+    for (int row = 0; row < boardSize; row++)
+    {
+        for (int col = 0; col < boardSize; col++)
+        {
+            board[row][col] = '~';
+        }
+    }
 }
 
 void startNewGame(char computerBoard[15][15], char playerBoard[15][15], int boardSize, bool manualInput, bool isPlayerTurn) {
@@ -57,5 +75,32 @@ void startNewGame(char computerBoard[15][15], char playerBoard[15][15], int boar
 
     if (!manualInput) {
         automaticallyFillBoard(playerBoard, boardSize);
+    } else {
+        //TODO: manual ship input, example: 0(first coord) 2(second coord) 0/1/true/false(is horizontal); validate placement
+    }
+}
+
+//TODO: format output(even spacing); print whole strings instead of each char in board
+void printUI(char computerBoard[15][15], char playerBoard[15][15], int boardSize) {
+    cout<<" ";
+    for (int col = 1; col <= boardSize; col++)
+    {
+        cout<<col<<" ";
+    }
+    cout<<endl;
+    
+    for (int row = 0; row < boardSize; row++)
+    {
+        cout<<row + 1<<" ";
+        for (int col = 0; col < boardSize; col++)
+        {
+            cout<<computerBoard[row][col]<<" ";
+        }
+        cout<<"| ";
+        for (int col = 0; col < boardSize; col++)
+        {
+            cout<<playerBoard[row][col]<<" ";
+        }
+        cout<<endl;
     }
 }
